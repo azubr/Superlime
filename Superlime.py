f9efa82c0b1eb937ae2b90a1a35ef52d132c9957
zubr <a@zubr.me>
2013-11-07 21:36:28 +0600

    changed license to GPLv3

--- Superlime.py
+++ Superlime.py
@@ -1,3 +1,19 @@
+# Superlime - Requests root/admin rights if a file cannot be saved in SublimeText
+# Copyright (C) 2013 Alexey Zubritsky
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sublime, sublime_plugin, subprocess, os, tempfile, re, binascii
 
 class Superlime(sublime_plugin.EventListener):
